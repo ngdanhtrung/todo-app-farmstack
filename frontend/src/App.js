7035e78278d7f74cd0a61b3ec0954db1a3c5d270
chanhhanh <chanhhanh23100@gmail.com>
2021-10-21 15:12:55 +0700

    added check button, GUI changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,111 +1,116 @@
-import "antd/dist/antd.css"
+import "antd/dist/antd.css";
 import "./App.css";
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CheckCircleOutlined, MinusCircleOutlined } from "@ant-design/icons"
-import { Col, Row, Timeline } from "antd"
+import { CheckCircleOutlined, MinusCircleOutlined } from "@ant-design/icons";
+import { Col, Row, Timeline } from "antd";
 import TodoView from "./components/TodoListView";
-
+import TodoItem from "./components/Todo";
 
 function App() {
-    const [todoList, setTodoList] = useState([{}])
-    const [timeline, setTimeline] = useState([])
-    const [title, setTitle] = useState("");
-    const [desc, setDesc] = useState("");
-    const urlRequest = `${process.env.REACT_APP_API_KEY}todos`;
+  const [todoList, setTodoList] = useState([{}]);
+  const [timeline, setTimeline] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [timelineControls, setTimelineControls] = useState([]);
+  const urlRequest = `${process.env.REACT_APP_API_KEY}todos`;
 
-    const getTodoList = () => {
-      axios.get(urlRequest).then((res) => {
-        setTodoList(res.data);
-        console.log(res.data);
-      });
-    };
+  const getTodoList = () => {
+    axios.get(urlRequest).then((res) => {
+      setTodoList(res.data);
+      console.log(res.data);
+    });
+  };
 
-    useEffect(getTodoList, []);
+  useEffect(getTodoList, []);
 
+  useEffect(() => {
+    const timelineItems = todoList.reverse().map((task) => {
+      return task.completed ? (
+        <Timeline.Item
+          dot={<CheckCircleOutlined />}
+          color='green'
+          style={{ textDecoration: "line-through", color: "green" }}
+          label={task.title}
+        >
+          {task.description}
+        </Timeline.Item>
+      ) : (
+        <Timeline.Item
+          dot={<MinusCircleOutlined />}
+          color='blue'
+          style={{ textDecoration: "initial" }}
+          label={task.title}
+        >
+          {task.description}
+        </Timeline.Item>
+      );
+    });
+    const timelineControlsItem = todoList.map((task) => {
+      return <TodoItem todo={task} getTodoList={getTodoList} />;
+    });
+    setTimelineControls(timelineControlsItem);
+    setTimeline(timelineItems);
+  }, [todoList]);
 
-    useEffect(() => {
-        const timelineItems = todoList.reverse().map((task) => {
-            return task.completed ? (
-              <Timeline.Item
-                dot={<CheckCircleOutlined />}
-                color='green'
-                style={{ textDecoration: "line-through", color: "green" }}
-              >
-                {task.title} <small>({task._id})</small> {task.description}
-              </Timeline.Item>
-            ) : (
-              <Timeline.Item
-                dot={<MinusCircleOutlined />}
-                color='blue'
-                style={{ textDecoration: "initial" }}
-              >
-                {task.title} <small>({task._id})</small> {task.description}
-              </Timeline.Item>
-            );
-        })
+  const addTodoHandler = async () => {
+    await axios
+      .post(urlRequest, {
+        title: title,
+        description: desc,
+        completed: false,
+      })
+      .then((res) => console.log(res));
+    getTodoList();
+  };
 
-        setTimeline(timelineItems)
-    }, [todoList])
-
-    const addTodoHandler = async () => {
-      await axios
-        .post(urlRequest, {
-          title: title,
-          description: desc,
-        })
-        .then((res) => console.log(res));
-      getTodoList();
-    };
-
-    return (
-      <div
-        className='App list-group-item justify-content-center align-items-center mx-auto'
-        style={{
-          width: "400px",
-          backgroundColor: "white",
-          marginTop: "15px",
-        }}
-      >
-        <div className='card-body'>
-          <h5 className='card text-white bg-dark mb-3'>Add your task</h5>
-          <span className='card-text'>
-            <input
-              className='mb-2 form-control titleIn'
-              onChange={(event) => setTitle(event.target.value)}
-              placeholder='Title'
-            />
-            <input
-              className='mb-2 form-control desIn'
-              onChange={(event) => setDesc(event.target.value)}
-              placeholder='Description'
-            />
-            <button
-              className='btn btn-outline-primary mx-2'
-              style={{
-                borderRadius: "50px",
-                "fort-weight": "bold",
-              }}
-              onClick={addTodoHandler}
-            >
-              Add task
-            </button>
-          </span>
-          <h5 className='card text-white bg-dark mb-3'>Your tasks</h5>
-          <div>
-            <TodoView todoList={todoList} />
-          </div>
-        </div>
-        {/*TimeLine
-        <Row style={{ marginTop: 50 }}>
-          <Col span={14} offset={5}>
+  return (
+    <div
+      className='App list-group-item justify-content-center align-items-center mx-auto'
+      style={{
+        width: "640px",
+        backgroundColor: "white",
+        marginTop: "15px",
+      }}
+    >
+      <div className='card-body'>
+        <h5 className='card text-white bg-dark mb-3'>Add your task</h5>
+        <span className='card-text'>
+          <input
+            className='mb-2 form-control titleIn'
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder='Title'
+          />
+          <input
+            className='mb-2 form-control desIn'
+            onChange={(event) => setDesc(event.target.value)}
+            placeholder='Description'
+          />
+          <button
+            className='btn btn-outline-primary mx-2'
+            style={{
+              borderRadius: "50px",
+              "fort-weight": "bold",
+            }}
+            onClick={addTodoHandler}
+          >
+            Add task
+          </button>
+        </span>
+        <h5 className='card text-white bg-dark mb-3'>Your tasks</h5>
+        <div>{/*<TodoView todoList={todoList} />*/}</div>
+      </div>
+      <div className='mt-5'>
+        <Row>
+          <Col span={12}>
             <Timeline mode='alternate'>{timeline}</Timeline>
           </Col>
-            </Row>
-        */}
+          <Col span={12}>{timelineControls}</Col>
+        </Row>
       </div>
-    );
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,32 +1,43 @@
 import axios from "axios";
 import React from "react";
-import { Button } from "antd"
-import { CloseOutlined } from "@ant-design/icons";
-
+import { Button, Tooltip } from "antd";
+import { DeleteOutlined, CheckOutlined } from "@ant-design/icons";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function TodoItem(props) {
   const urlRequest = `${process.env.REACT_APP_API_KEY}todos`;
   const deleteTodoHandler = (_id) => {
-    axios.delete(`${urlRequest}/${_id}`)
-    .then(res=> console.log(res.data))
-  }
+    axios.delete(`${urlRequest}/${_id}`).then((res) => console.log(res.data));
+    props.getTodoList();
+  };
+  const completeTodoHandler = (_id) => {
+    axios
+      .put(`${urlRequest}/${_id}`, { completed: true })
+      .then((res) => console.log(res.data));
+    props.getTodoList();
+  };
   return (
-    <div>
-      <p>
-        <span style={{ fontWeight: "bold, underline" }}>
-          {props.todo.title} :
-        </span>
-        {props.todo.description}
-        <Button
-          onClick={() => deleteTodoHandler(props.todo._id)}
-          danger
-          type='primary'
-          shape='circle'
-          icon={<CloseOutlined />}
-        >
-        </Button>
-        <hr></hr>
-      </p>
+    <div className='d-flex flex-row justify-content-end mb-2 me-5'>
+      <div className='mx-1'>
+        <Tooltip title='Complete'>
+          <Button
+            onClick={() => completeTodoHandler(props.todo._id)}
+            shape='circle'
+            icon={<CheckOutlined />}
+          />
+        </Tooltip>
+      </div>
+      <div className='mx-1'>
+        <Tooltip title='Delete'>
+          <Button
+            onClick={() => deleteTodoHandler(props.todo._id)}
+            shape='circle'
+            danger
+            ghost
+            icon={<DeleteOutlined />}
+          />
+        </Tooltip>
+      </div>
     </div>
   );
 }
